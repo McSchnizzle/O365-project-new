14907c9fab501682c5aff64de53d30cdd98f6517
Paul Brown <paul@teamcinder.com>
2025-03-19 15:18:04 -0700

    cleaned up files a bit

--- Modularized code-works before second database/main.py
+++
@@ -1,4 +0,0 @@
-from sync_email import main
-
-if __name__ == "__main__":
-    main()
--- Modularized code/auth.py
+++ Modularized code/auth.py
@@ -1,5 +1,12 @@
-import os, json, msal
+import os
+import json
+import msal
+import certifi
 from config import CLIENT_ID, AUTHORITY, TOKEN_CACHE_FILE, SYNC_SCOPES
+
+# For testing, we set the environment variables so that requests uses certifi’s bundle.
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 def get_token(scopes):
     cache = msal.SerializableTokenCache()
@@ -12,11 +19,11 @@ def get_token(scopes):
     if accounts:
         result = app.acquire_token_silent(scopes, account=accounts[0])
     if not result:
-        flow = app.initiate_device_flow(scopes=scopes)
+        flow = app.initiate_device_flow(scopes=scopes, verify=False)  # Disable verification here for testing.
         if "user_code" not in flow:
             raise Exception("Failed to create device flow: " + json.dumps(flow, indent=4))
         print(flow["message"])
-        result = app.acquire_token_by_device_flow(flow)
+        result = app.acquire_token_by_device_flow(flow, verify=False)
     if cache.has_state_changed:
         with open(TOKEN_CACHE_FILE, "w") as f:
             f.write(cache.serialize())
